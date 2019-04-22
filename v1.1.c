68075558707c5be664840747f1488811179346da
roshan395 <32610544+roshan395@users.noreply.github.com>
2019-04-22 20:55:10 +0530

    Update v1.1.c

--- v1.1.c
+++ v1.1.c
@@ -1,1 +1,29 @@
-afda
+ #include <stdio.h>
+    int main ()
+    {
+ 
+        int number[10];
+ 
+        int i, j, a, n;
+        n=10;
+        char s;
+        char name[11] = {'a','b','c','d','e','f','g','h','i','j','\0'};     
+ 
+        printf("Enter the number of gifts \nstudent\tgifts\n");
+        for (i = 0; i < n; i++)
+        {
+			printf("%c\t: ",name[i]);
+	        scanf("%d", &number[i]);
+	    }
+ 
+       
+ 
+        printf("order of billed students are given below\nstudent\tgift\n");
+ 
+        for (i = 0; i < n; ++i) 
+        {
+        	printf("%c\t: ",name[i]);
+            printf("%d\n", number[i]);
+        }
+ return 0;
+    }
